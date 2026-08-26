7355cf6558a61fcddd8ac627b5607d0b178ea3bf
Akhil Vikram Singh <sakhilvikram@gmail.com>
2026-08-26 15:44:05 +0530

    feat(services): add experimental Gemini text embedding function

--- services/embadding.py
+++ services/embadding.py
@@ -16,3 +16,23 @@ def get_model():
         _model_instance = SentenceTransformer("all-MiniLM-L6-v2")
     return _model_instance
 
+def _gemini_text_to_vector(document: str):
+    """ this function user for conver long text into small chunks and srote them to a vector database"""
+    client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+    
+    try:
+        embaddings = []
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=0)
+        chunks = text_splitter.split_text(document)
+        for chunk in chunks:
+            response = client.models.embed_content(
+                model="gemini-embedding-001",
+                contents=chunk
+            )
+            embaddings.append(response.embeddings[0].values)
+    except Exception as e:
+        print(f"Error:{e}")
+        embaddings = []
+
+    return embaddings
+
