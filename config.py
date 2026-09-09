53c0cdd6a5fccda96c95b9cbecbd1793ea1895f9
Akhil Vikram Singh <sakhilvikram@gmail.com>
2026-09-09 18:07:17 +0530

    config: add anonymous telemetry suppression for vector database startup

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
-
 from dotenv import load_dotenv
+
 load_dotenv()
+
+# Prevent ChromaDB telemetry network delays on startup
+os.environ.setdefault("ANONYMOUS_TELEMETRY", "False")
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 HF_TOKEN = os.getenv("HF_TOKEN")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
